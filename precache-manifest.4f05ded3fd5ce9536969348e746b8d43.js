bcc4ed1973835c94455c96e6c3479d2e78c5d1bc
YosorIT <yosorit@gmail.com>
2020-09-13 23:55:54 +0300

    Updates

---
+++ precache-manifest.4f05ded3fd5ce9536969348e746b8d43.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020845249c6336abddb82198ef0f334f",
+    "url": "/hillel-react-hw-1/index.html"
+  },
+  {
+    "revision": "63babfc6899f190dc55f",
+    "url": "/hillel-react-hw-1/static/css/main.d0ee8c21.chunk.css"
+  },
+  {
+    "revision": "f646354e90c2f0148974",
+    "url": "/hillel-react-hw-1/static/js/2.e5aa64a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hillel-react-hw-1/static/js/2.e5aa64a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63babfc6899f190dc55f",
+    "url": "/hillel-react-hw-1/static/js/main.7e23860a.chunk.js"
+  },
+  {
+    "revision": "1331cb8934bfc2c09b78",
+    "url": "/hillel-react-hw-1/static/js/runtime-main.89c32708.js"
+  },
+  {
+    "revision": "1734a8c8e9205636993d40cbac4d9c2e",
+    "url": "/hillel-react-hw-1/static/media/bg.1734a8c8.jpg"
+  },
+  {
+    "revision": "08129180936c16f4bffca2351597e534",
+    "url": "/hillel-react-hw-1/static/media/img-1.08129180.jpg"
+  },
+  {
+    "revision": "cc8eecc80379720c6b830fdc2f3d5743",
+    "url": "/hillel-react-hw-1/static/media/img-2.cc8eecc8.jpg"
+  },
+  {
+    "revision": "c56567ae0b6818e2cfcadc8447f2c703",
+    "url": "/hillel-react-hw-1/static/media/img-3.c56567ae.jpg"
+  },
+  {
+    "revision": "ff37677df4b9f9124faa479eb73bbcec",
+    "url": "/hillel-react-hw-1/static/media/img-4.ff37677d.jpg"
+  },
+  {
+    "revision": "a700596cea9d048dea9dc5ecd8732973",
+    "url": "/hillel-react-hw-1/static/media/img-5.a700596c.jpg"
+  },
+  {
+    "revision": "b4b4c8ddc465e6e2490190ab26bb73d1",
+    "url": "/hillel-react-hw-1/static/media/img-6.b4b4c8dd.jpg"
+  }
+]);
